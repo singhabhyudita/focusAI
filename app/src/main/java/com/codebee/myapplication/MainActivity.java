3353cdbd900e0bed2ad6779d94d5e25d195fc939
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2022-04-23 13:31:02 +0530

    added location services

--- app/src/main/java/com/codebee/myapplication/MainActivity.java
+++ app/src/main/java/com/codebee/myapplication/MainActivity.java
@@ -1,13 +1,24 @@
 package com.codebee.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 
     private Button startButton, stopButton;
+
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
 
     private boolean isMotionSensorActive = false;
     private MotionSensor motionSensor = new MotionSensor(MainActivity.this);
@@ -25,6 +36,17 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 motionSensor.startSensor();
+
+//                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                    ActivityCompat.requestPermissions(
+//                            MainActivity.this,
+//                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                            REQUEST_CODE_LOCATION_PERMISSION
+//                    );
+//                }else{
+//                    startLocationService();
+//                }
+
             }
         });
 
@@ -32,7 +54,54 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 motionSensor.stopSensor();
+
+//                stopLocationService();
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                startLocationService();
+            }else{
+                Toast.makeText(this, "Location permission denied!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean isLocationServiceRunning() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        if(activityManager != null) {
+            for(ActivityManager.RunningServiceInfo service : activityManager.getRunningServices(Integer.MAX_VALUE)){
+                if(LocationService.class.getName().equals(service.service.getClassName())){
+                    if(service.foreground){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private void startLocationService() {
+        if(!isLocationServiceRunning()){
+            Intent intent = new Intent(getApplicationContext(), LocationService.class);
+            intent.setAction(Constants.ACTION_START_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this, "Location service started!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void stopLocationService() {
+        if(isLocationServiceRunning()){
+            Intent intent = new Intent(getApplicationContext(), LocationService.class);
+            intent.setAction(Constants.ACTION_STOP_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this, "Location service stopped!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/codebee/myapplication/MotionSensor.java
+++ app/src/main/java/com/codebee/myapplication/MotionSensor.java
@@ -1,19 +1,20 @@
 package com.codebee.myapplication;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 
-import com.codebee.myapplication.ml.MobilenetV110224Quantized1Default1;
-
-import org.tensorflow.lite.DataType;
-import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
-
-import java.io.IOException;
-import java.nio.ByteBuffer;
+import androidx.core.app.NotificationCompat;
 
 public class MotionSensor extends Thread implements SensorEventListener {
 
@@ -25,14 +26,12 @@ public class MotionSensor extends Thread implements SensorEventListener {
     private final Context context;
     private volatile boolean stopThread = false;
 
-    float[] xValuesLeft = new float[40];
-    float[] xValuesRight = new float[40];
-    float[] yValuesLeft = new float[40];
-    float[] yValuesRight = new float[40];
-    float[] zValuesLeft = new float[40];
-    float[] zValuesRight = new float[40];
+    private boolean firstData = true;
+    float[][] valuesLeft = new float[40][3];
+    float[][] valuesRight = new float[40][3];
 
     private int counter = 0;
+    private int sentCount = 0;
     long lastSaved = System.currentTimeMillis();
 
     public MotionSensor(Context context) {
@@ -48,6 +47,8 @@ public class MotionSensor extends Thread implements SensorEventListener {
 
     public void startSensor() {
         counter = 0;
+        sentCount = 0;
+        firstData = true;
         sensorManager.registerListener(MotionSensor.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         Log.d(TAG, "Registered accelerometer listener");
     }
@@ -69,35 +70,79 @@ public class MotionSensor extends Thread implements SensorEventListener {
 
             if(counter == 40){
                 counter = 0;
-                System.arraycopy(xValuesRight, 0, xValuesLeft, 0, 40);
-                System.arraycopy(yValuesRight, 0, yValuesLeft, 0, 40);
-                System.arraycopy(zValuesRight, 0, zValuesLeft, 0, 40);
-                System.out.println("Send data...");
+                for(int i = 0; i < 40; i++)
+                {
+                    valuesLeft[i][0] = valuesRight[i][0];
+                    valuesLeft[i][1] = valuesRight[i][1];
+                    valuesLeft[i][2] = valuesRight[i][2];
+                }
+
+                if(!firstData) {
+                    System.out.println("Send data...");
+
+                    // TODO: Get class from model
+                    sentCount++;
+                    if(sentCount == 3){
+                        stopSensor();
+
+                        double pSafe = Math.random();
+                        double pDrunk = Math.random();
+                        double pUnsafe = 1 - pSafe;
+                        if(pDrunk > 0.7 || pUnsafe > 0.6) {
+                            final Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+                            final VibrationEffect vibrationEffect1;
+                            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                                vibrationEffect1 = VibrationEffect.createOneShot(5000, VibrationEffect.EFFECT_HEAVY_CLICK);
+                                vibrator.cancel();
+                                vibrator.vibrate(vibrationEffect1);
+                            }
+
+                            String channelId = "motion_sensor_notification_channel";
+                            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                            Intent resultIntent = new Intent();
+                            PendingIntent pendingIntent = PendingIntent.getActivity(context.getApplicationContext(), 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                            NotificationCompat.Builder builder = new NotificationCompat.Builder(context.getApplicationContext(), channelId);
+                            builder.setSmallIcon(R.mipmap.ic_launcher);
+                            builder.setContentTitle("FocusAI");
+                            builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+                            builder.setContentText("Safety Alert!");
+                            builder.setAutoCancel(false);
+                            builder.setPriority(NotificationCompat.PRIORITY_MAX);
+
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                if (notificationManager != null && notificationManager.getNotificationChannel(channelId) == null) {
+                                    NotificationChannel notificationChannel = new NotificationChannel(
+                                            channelId,
+                                            "Motion Sensor Service",
+                                            NotificationManager.IMPORTANCE_HIGH
+                                    );
+                                    notificationChannel.setDescription("This channel is used by motion sensor service.");
+                                    notificationManager.createNotificationChannel(notificationChannel);
+                                }
+                            }
+
+                            notificationManager.notify(1, builder.build());
+                        }
+
+                        long t = (long) Math.ceil(1000 * 60 * 2 * pSafe);
+                        try {
+                            System.out.println("Sleeping for " + t + " ms");
+                            Thread.sleep(t);
+                            startSensor();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return;
+                    }
+                }
+                else
+                    firstData = false;
             }
 
-//            try {
-//                MobilenetV110224Quantized1Default1 model = MobilenetV110224Quantized1Default1.newInstance(context);
-//
-//                ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4*3*80);
-//
-//                // Creates inputs for reference.
-//                TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.UINT8);
-//                inputFeature0.loadBuffer(byteBuffer);
-//
-//                // Runs model inference and gets result.
-//                MobilenetV110224Quantized1Default1.Outputs outputs = model.process(inputFeature0);
-//                TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
-//
-//                // Releases model resources if no longer used.
-//                model.close();
-//            } catch (IOException e) {
-//                // TODO Handle the exception
-//            }
-
-
-            xValuesRight[counter] = event.values[0];
-            yValuesRight[counter] = event.values[1];
-            zValuesRight[counter] = event.values[2];
+            valuesRight[counter][0] = event.values[0];
+            valuesRight[counter][1] = event.values[1];
+            valuesRight[counter][1] = event.values[2];
             counter++;
         }
     }
